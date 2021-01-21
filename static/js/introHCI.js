57e92075796c24c6c1ce9530f170319790c29763
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2021-01-20 20:39:44 -0800

    new

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -4,6 +4,16 @@
 $(document).ready(function() {
 	initializePage();
 })
+function projectClick(e){
+	var containingProject = $(this).closest(".project");
+	e.preventDefault();
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.toggle();
+    }
+}
 
 /*
  * Function that is called when the document is ready.
@@ -11,8 +21,11 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
-
+	
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
